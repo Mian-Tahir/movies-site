fc5ee6ea7046098a9f2c7cbea7ea238957e98099
Mian-Tahir <miantahir1827@gmail.com>
2023-10-02 10:57:23 +0500

    movies site

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector(".navbar form");
+  
+    form.addEventListener("submit", function (event) {
+      event.preventDefault(); 
+    });
+  
+  });
+  
+
+function searchMovies (){
+   const searchString = document.getElementById("movieName").value ;
+    const API_KEY = "a8af5a30"
+    const API_URL = `http://www.omdbapi.com/?S=${searchString}&apikey=${API_KEY}`
+
+    const apiCall = fetch(API_URL);
+    apiCall.then((movie)=>{
+        return movie.json()
+    }).then((data)=>{
+        const movies = data.Search.filter((movie)=>{
+return movie.Year >= document.getElementById('year').value
+        });
+        const outPut = movies.map((movie)=>{
+            return (`<img src=${movie.Poster} />` )
+        });
+
+        document.getElementById('demo').innerHTML=outPut.join(" ")
+
+    })
+ 
+}
+
+const btn = document.getElementById('btn')
+btn.onclick=searchMovies;
